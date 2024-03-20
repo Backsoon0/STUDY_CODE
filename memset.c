476c44962faa509dcc6a5037b2f66f1190d8773b
Backoon <ggmashanghuilai@gmail.com>
2024-03-20 15:44:34 +0800

    deleted

---
+++ memset.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int num[30];
+    memset(num,0,sizeof(num));
+    return 0;
+}
--- robocom/2/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    char *s="\t\\Name\\Address\n";
-    printf("%s",s);
-}
